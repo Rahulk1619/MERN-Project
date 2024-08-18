2b929e2918b5d0f6f09b5608f02ca486269db0f0
Rahulk1619 <111602826+Rahulk1619@users.noreply.github.com>
2024-08-18 17:09:54 +0530

    Add files via upload

---
+++ component/Footer.jsx
@@ -0,0 +1,14 @@
+
+import "./Footer.css";
+
+export const Footer = () =>{
+    return(
+        <>
+        <footer>
+            <div className="container-footer">
+                <h2>@RkUniversal 2024</h2>
+            </div>
+        </footer>
+        </>
+    )
+}
